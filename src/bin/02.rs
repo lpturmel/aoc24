a29d69a2213ec72655e61dec392ab2a9194895ab
lpturmel <louisphilippe.turmel@gmail.com>
2024-12-02 11:14:54 -0500

    Added day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,24 +1,68 @@
 use aoc24::{get_input, Day};
 
 #[derive(Debug)]
-struct DayTwo {}
+struct DayTwo {
+    data: Vec<Vec<usize>>,
+}
+
+impl DayTwo {
+    fn is_valid(row: &[usize]) -> bool {
+        let diffs = row
+            .windows(2)
+            .map(|w| w[1] as isize - w[0] as isize)
+            .collect::<Vec<_>>();
+
+        let first_diff = diffs[0];
+
+        let is_increasing = first_diff > 0;
+        let is_valid_diff = |&d: &isize| d != 0 && d.abs() <= 3;
+
+        diffs
+            .iter()
+            .all(|&d| is_valid_diff(&d) && (d > 0) == is_increasing)
+    }
+
+    fn is_valid_one_removed(row: &[usize]) -> bool {
+        (0..row.len()).any(|i| {
+            let new_row = row
+                .iter()
+                .enumerate()
+                .filter_map(|(j, &val)| if j != i { Some(val) } else { None })
+                .collect::<Vec<_>>();
+            Self::is_valid(&new_row)
+        })
+    }
+}
 
 impl Day<usize> for DayTwo {
     fn part_one(&self) -> usize {
-        todo!()
+        self.data.iter().filter(|row| Self::is_valid(row)).count()
     }
     fn part_two(&self) -> usize {
-        todo!()
+        self.data
+            .iter()
+            .filter(|row| Self::is_valid(row) || Self::is_valid_one_removed(row))
+            .count()
     }
 
     fn from_input() -> Result<Self, std::io::Error> {
         let data = get_input(2)?;
-        todo!()
+
+        let data: Vec<Vec<usize>> = data
+            .lines()
+            .map(|line| {
+                line.split_whitespace()
+                    .filter_map(|s| s.parse::<usize>().ok())
+                    .collect()
+            })
+            .collect();
+
+        Ok(DayTwo { data })
     }
 }
 fn main() -> Result<(), std::io::Error> {
-    let day_one = DayTwo::from_input()?;
-    aoc24::run(day_one)?;
+    let day_two = DayTwo::from_input()?;
+    aoc24::run(day_two)?;
 
     Ok(())
 }
